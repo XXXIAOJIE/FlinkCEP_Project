a3a728b38e0e9623a1799185376a7861b1c908ed
arianeziehn <ariane.ziehn@campus.tu-berlin.de>
2024-07-29 12:30:21 +0200

    update App class to represent patterns transform times (iteration operator) into seq, i.e., 2 seq to cover and conditions of the pattern

--- src/main/java/example/App.java
+++ src/main/java/example/App.java
@@ -2,8 +2,8 @@ package example;
 
 /**
  * Hello world!
- *
  */
+
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.Serializer;
 import com.esotericsoftware.kryo.io.Input;
@@ -12,6 +12,7 @@ import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringEncoder;
+import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.cep.CEP;
 import org.apache.flink.cep.PatternSelectFunction;
 import org.apache.flink.cep.PatternStream;
@@ -28,6 +29,7 @@ import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSin
 import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
+import scala.Tuple2;
 
 import java.net.URI;
 import java.text.ParseException;
@@ -40,51 +42,35 @@ import static org.apache.flink.streaming.api.TimeCharacteristic.EventTime;
 
 public class App {
 
-    public static void main(String[] args) throws Exception {
-        DataStream<String> result1 = Taxi();
-       // DataStream<String> result2 = Sensor();
-//        DataStream<Tuple2<KeyedDataPointGeneral, KeyedDataPointGeneral>> result = patternStream.flatSelect(new UDFs.GetResultTuple2());
-//        result.flatMap(new LatencyLoggerT2(true));
-//        result.writeAsText(outputPath, FileSystem.WriteMode.OVERWRITE);
-    }
-
     public static class URISerializer extends Serializer<URI> {
         @Override
         public void write(Kryo kryo, Output output, URI uri) {
             output.writeString(uri.toString());
         }
+
         @Override
         public URI read(Kryo kryo, Input input, Class<URI> aClass) {
             return URI.create(input.readString());
         }
     }
 
-
-    static DataStream<String> Taxi() throws Exception {
-        //BasicConfigurator.configure();
+    public static void main(String[] args) throws Exception {
+        // create stream environment
         // 设置执行环境
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setStreamTimeCharacteristic(EventTime);
 
-        // 注册自定义Kryo序列化器
-        env.getConfig().registerTypeWithKryoSerializer(URI.class, URISerializer.class);
-
-        /** WatermarkStrategy<TaxiRide> orderWatermarkStrategy = CustomWatermarkStrategy
-               * .<TaxiRide>forBoundedOutOfOrderness(Duration.ofSeconds(1))
-                 .withTimestampAssigner((element, timestamp) ->
-                         element.getPickupDatetime(); // TODO that needs to be milliseconds not Data
-                 );*/
-
+        // define watermark strategy for TaxiRide data point = element.getPickupDatetime().getTime());
         WatermarkStrategy<TaxiRide> orderWatermarkStrategy = WatermarkStrategy
                 .<TaxiRide>forBoundedOutOfOrderness(Duration.ofSeconds(1))
                 .withTimestampAssigner((element, timestamp) ->
                         element.getPickupDatetime().getTime());
 
-
-        // read yellow taxi data
+        // read yellow taxi data from CSV file
         DataStream<TaxiRide> yellowTaxiRides = env.readTextFile("taxi_data/yellow_tripdata_2020-05(5am-8am).csv")
                 .flatMap(new FlatMapFunction<String, TaxiRide>() {
                     private boolean isFirstLine = true;
+
                     @Override
                     public void flatMap(String s, Collector<TaxiRide> collector) throws ParseException {
                         if (isFirstLine) {
@@ -96,14 +82,14 @@ public class App {
                             collector.collect(ride);
                         }
                     }
-                })
-                .assignTimestampsAndWatermarks(orderWatermarkStrategy);
+                });
 
 
-        // read green Taxi data
+        // read green Taxi data from CSV file
         DataStream<TaxiRide> greenTaxiRides = env.readTextFile("taxi_data/green_tripdata_2020-05(5am-8am).csv")
                 .flatMap(new FlatMapFunction<String, TaxiRide>() {
                     private boolean isFirstLine = true;
+
                     @Override
                     public void flatMap(String s, Collector<TaxiRide> collector) throws ParseException {
                         if (isFirstLine) {
@@ -115,81 +101,103 @@ public class App {
                             collector.collect(ride);
                         }
                     }
-                })
-                .assignTimestampsAndWatermarks(orderWatermarkStrategy);
+                });
 
         // 将模式应用到数据流
-        DataStream<TaxiRide> allTaxiRides = yellowTaxiRides.union(greenTaxiRides);
+        // combine both streams for patterns, assign the timestamp and the key (for condition vendorid == vendorid
+        DataStream<TaxiRide> allTaxiRides = yellowTaxiRides.union(greenTaxiRides)
+                        .assignTimestampsAndWatermarks(orderWatermarkStrategy)
+                .keyBy(new KeySelector<TaxiRide, Double>() {
+                    @Override
+                    public Double getKey(TaxiRide taxiRide) throws Exception {
+                        return taxiRide.getVendorID();
+                    }
+                });;
 
         // Pattern: triggering a CE when yellow and green taxis with vendorID 1 appear simultaneously within a ten-minute window
+        // The pattern is a bit fuzy please write in PSL else natural language is tricky; I understood:
+        /** PATTERN AND(Yellow, Green)
+         * WHERE id = id AND id = 1.0
+         * WINDOW 10 minutes
+         */
+        // Flink has no and only SEQ thus you need SEQ (Yellow, GReen) and SEQ (Green, Yellow), id = id is automatically handled by keyby for key = 1.0 you need a filter condition
         AfterMatchSkipStrategy skipStrategy = AfterMatchSkipStrategy.noSkip();
-        Pattern<TaxiRide, ?> pattern = Pattern.<TaxiRide>begin("start", skipStrategy)
-                .where(new IterativeCondition<TaxiRide>() {
+        // pattern1: SEQ (Yellow, Green)
+        Pattern<TaxiRide, ?> pattern1 = Pattern.<TaxiRide>begin("start", skipStrategy)
+                .where(new SimpleCondition<TaxiRide>(){
+
                     @Override
-                    public boolean filter(TaxiRide ride, Context<TaxiRide> ctx) throws Exception {
-                        return (ride.getColor().equals("yellow") && ride.getVendorID() == 1 || ride.getColor().equals("green")) && ride.getVendorID() == 1;
+                    public boolean filter(TaxiRide taxiRide) throws Exception {
+                        if (taxiRide.getColor().equals("yellow") && taxiRide.getVendorID() == 1.0){
+                            return true;
+                        }
+                        return false;
                     }
-                })
-                .times(2)  // 期待两个匹配事件
-//                .where(new SimpleCondition<TaxiRide>() {
-//                    @Override
-//                    public boolean filter(TaxiRide ride) {
-//                        return ride.getColor().equals("yellow") && ride.getVendorID() == 1;
-//                    }
-//                })
-//                .where(new SimpleCondition<TaxiRide>() {
-//                    @Override
-//                    public boolean filter(TaxiRide ride) {
-//                        return ride.getColor().equals("green") && ride.getVendorID() == 1;
-//                    }
-//                })
-                .within(Time.minutes(10));
+                }).followedByAny("followedBy")
+                .where(new SimpleCondition<TaxiRide>(){
+
+                    @Override
+                    public boolean filter(TaxiRide taxiRide) throws Exception {
+                        if (taxiRide.getColor().equals("green") && taxiRide.getVendorID() == 1.0 ){
+                            return true;
+                        }
+                        return false;
+                    }
+                }).within(Time.minutes(10));
+
+        // pattern2: SEQ (Green, Yellow)
+        Pattern<TaxiRide, ?> pattern2 = Pattern.<TaxiRide>begin("start", skipStrategy)
+                .where(new SimpleCondition<TaxiRide>(){
+
+                    @Override
+                    public boolean filter(TaxiRide taxiRide) throws Exception {
+                        if (taxiRide.getColor().equals("green") && taxiRide.getVendorID() == 1.0){
+                            return true;
+                        }
+                        return false;
+                    }
+                }).followedByAny("followedBy")
+                .where(new SimpleCondition<TaxiRide>(){
+
+                    @Override
+                    public boolean filter(TaxiRide taxiRide) throws Exception {
+                        if (taxiRide.getColor().equals("yellow") && taxiRide.getVendorID() == 1.0){
+                            return true;
+                        }
+                        return false;
+                    }
+                }).within(Time.minutes(10));
 
         // 将模式应用到数据流上
-        //PatternStream<TaxiRide> patternStream = CEP.pattern(allTaxiRides.keyBy(TaxiRide::getColor), pattern);
-        PatternStream<TaxiRide> patternStream = CEP.pattern(allTaxiRides, pattern);
+        PatternStream<TaxiRide> patternStreamYellowGreen = CEP.pattern(allTaxiRides, pattern1);
+        PatternStream<TaxiRide> patternStreamGreenYellow = CEP.pattern(allTaxiRides, pattern2);
 
 
         // 从匹配的模式中选择结果
-        DataStream<String> result = patternStream.process(new PatternProcessFunction<TaxiRide, String>() {
-            @Override//Called every time a matching sequence of events is found.
-            public void processMatch(Map<String, List<TaxiRide>> match, Context context, Collector<String> collector) throws Exception {
-                StringBuilder matches = new StringBuilder();
-                List<TaxiRide> rides = match.get("start");
-                TaxiRide yellowRide = rides.get(0);
-                TaxiRide greenRide = rides.get(1);
-
-                String s = "Yellow taxi pickup at_" + yellowRide.getPickupDatetime() + "_drop at_" + yellowRide.getDropoffDatetime() +
-                        "_with Green taxi at_" + greenRide.getPickupDatetime() + "_drop at_" + greenRide.getDropoffDatetime();
-                collector.collect(s);
+        DataStream<Tuple2<TaxiRide,TaxiRide>> resultYellowGreen = patternStreamYellowGreen.process(new PatternProcessFunction<TaxiRide, Tuple2<TaxiRide,TaxiRide>>() {
+            @Override
+            public void processMatch(Map<String, List<TaxiRide>> match, Context context, Collector<Tuple2<TaxiRide, TaxiRide>> collector) throws Exception {
+                TaxiRide StartRide = match.get("start").get(0);
+                TaxiRide EndRide = match.get("followedBy").get(0);
+                collector.collect(new Tuple2<TaxiRide,TaxiRide>(StartRide,EndRide));
             }
         });
 
-        result.print();
-        String outputPath = "taxi_result/result1.csv";
-        result.writeAsText(outputPath, FileSystem.WriteMode.OVERWRITE);
-        //create csv datei
-//        final StreamingFileSink<String> sink = StreamingFileSink
-//                .forRowFormat(new Path(outputPath), new SimpleStringEncoder<String>("UTF-8"))
-//                .withRollingPolicy(DefaultRollingPolicy.builder()
-//                        .withRolloverInterval(Duration.ofMinutes(1_000).toMillis())
-//                        .withInactivityInterval(Duration.ofMinutes(1_000).toMillis())
-//                        .withMaxPartSize(1024 * 1024 * 1024)
-//                        .build())
-//                .build();
-//
-//        result.addSink(sink);
+        DataStream<Tuple2<TaxiRide,TaxiRide>> resultGreenYellow = patternStreamGreenYellow.process(new PatternProcessFunction<TaxiRide, Tuple2<TaxiRide,TaxiRide>>() {
+            @Override
+            public void processMatch(Map<String, List<TaxiRide>> match, Context context, Collector<Tuple2<TaxiRide, TaxiRide>> collector) throws Exception {
+                TaxiRide StartRide = match.get("start").get(0);
+                TaxiRide EndRide = match.get("followedBy").get(0);
+                collector.collect(new Tuple2<TaxiRide,TaxiRide>(StartRide,EndRide));
+            }
+        });
 
+        DataStream<Tuple2<TaxiRide,TaxiRide>> result = resultGreenYellow.union(resultYellowGreen);
+        String outputPath = "taxi_result/result.csv";
+        result.writeAsText(outputPath, FileSystem.WriteMode.OVERWRITE).setParallelism(1);
+        // this works for me the results look good to me as well
         // 执行Flink作业
         env.execute("Taxi Ride CEP Example");
-        return result;
     }
-
-
-    private static DataStream<String> Sensor() {
-        DataStream<String> result = null;
-        return result;
-    }
-
 
 }
