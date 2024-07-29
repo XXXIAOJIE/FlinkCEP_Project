07d6c4c5fc7d82d554fab24c701f318d45374f6f
xiaojiesun <dadaciyou@gmail.com>
2024-07-29 07:13:08 +0200

    save result as text

--- src/main/java/example/App.java
+++ src/main/java/example/App.java
@@ -4,37 +4,59 @@ package example;
  * Hello world!
  *
  */
-import org.apache.flink.api.common.eventtime.Watermark;
-import org.apache.flink.api.common.eventtime.WatermarkGenerator;
-import org.apache.flink.api.common.eventtime.WatermarkOutput;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
 import org.apache.flink.cep.CEP;
 import org.apache.flink.cep.PatternSelectFunction;
 import org.apache.flink.cep.PatternStream;
+import org.apache.flink.cep.functions.PatternProcessFunction;
+import org.apache.flink.cep.nfa.aftermatch.AfterMatchSkipStrategy;
 import org.apache.flink.cep.pattern.Pattern;
+import org.apache.flink.cep.pattern.conditions.IterativeCondition;
 import org.apache.flink.cep.pattern.conditions.SimpleCondition;
+import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
+import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
 
+import java.net.URI;
 import java.text.ParseException;
 import java.time.Duration;
 import java.util.List;
 import java.util.Map;
+//import java.util.concurrent.TimeUnit;
 
 import static org.apache.flink.streaming.api.TimeCharacteristic.EventTime;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-        //System.out.println("hello");
         DataStream<String> result1 = Taxi();
        // DataStream<String> result2 = Sensor();
 //        DataStream<Tuple2<KeyedDataPointGeneral, KeyedDataPointGeneral>> result = patternStream.flatSelect(new UDFs.GetResultTuple2());
-//
 //        result.flatMap(new LatencyLoggerT2(true));
 //        result.writeAsText(outputPath, FileSystem.WriteMode.OVERWRITE);
+    }
+
+    public static class URISerializer extends Serializer<URI> {
+        @Override
+        public void write(Kryo kryo, Output output, URI uri) {
+            output.writeString(uri.toString());
+        }
+        @Override
+        public URI read(Kryo kryo, Input input, Class<URI> aClass) {
+            return URI.create(input.readString());
+        }
     }
 
 
@@ -44,77 +66,119 @@ public class App {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setStreamTimeCharacteristic(EventTime);
 
-       /** WatermarkStrategy<TaxiRide> orderWatermarkStrategy = CustomWatermarkStrategy
-              * .<TaxiRide>forBoundedOutOfOrderness(Duration.ofSeconds(1))
-                .withTimestampAssigner((element, timestamp) ->
-                        element.getPickupDatetime(); // TODO that needs to be milliseconds not Data
-                );*/
-        // 设置自定义的Watermark策略
+        // 注册自定义Kryo序列化器
+        env.getConfig().registerTypeWithKryoSerializer(URI.class, URISerializer.class);
+
+        /** WatermarkStrategy<TaxiRide> orderWatermarkStrategy = CustomWatermarkStrategy
+               * .<TaxiRide>forBoundedOutOfOrderness(Duration.ofSeconds(1))
+                 .withTimestampAssigner((element, timestamp) ->
+                         element.getPickupDatetime(); // TODO that needs to be milliseconds not Data
+                 );*/
+
         WatermarkStrategy<TaxiRide> orderWatermarkStrategy = WatermarkStrategy
                 .<TaxiRide>forBoundedOutOfOrderness(Duration.ofSeconds(1))
                 .withTimestampAssigner((element, timestamp) ->
-                        element.getPickupDatetime().getTime());   //As far as I know getTime returns results in milliseconds
+                        element.getPickupDatetime().getTime());
 
 
-        // 创建一个数据流
-        DataStream<TaxiRide> yellowTaxiRides = env.readTextFile("data/yellow_tripdata_2020-05(5am-8am).csv")
-                .map(new MapFunction<String, TaxiRide>() {
+        // read yellow taxi data
+        DataStream<TaxiRide> yellowTaxiRides = env.readTextFile("taxi_data/yellow_tripdata_2020-05(5am-8am).csv")
+                .flatMap(new FlatMapFunction<String, TaxiRide>() {
+                    private boolean isFirstLine = true;
                     @Override
-                    public TaxiRide map(String value) throws ParseException {
-                        String[] fields = value.split(",");
-                        return new TaxiRide("yellow", Double.parseDouble(fields[0]), fields[1], fields[2],
-                                Double.parseDouble(fields[8]), Double.parseDouble(fields[16]), 0);
+                    public void flatMap(String s, Collector<TaxiRide> collector) throws ParseException {
+                        if (isFirstLine) {
+                            isFirstLine = false;
+                        } else {
+                            String[] fields = s.split(";");
+                            TaxiRide ride = new TaxiRide("yellow", Double.parseDouble(fields[0]), fields[1], fields[2],
+                                    Double.parseDouble(fields[8]), Double.parseDouble(fields[16]), 0);
+                            collector.collect(ride);
+                        }
                     }
-                }).assignTimestampsAndWatermarks(orderWatermarkStrategy);
+                })
+                .assignTimestampsAndWatermarks(orderWatermarkStrategy);
 
 
-        // 读取绿色出租车数据流
-        DataStream<TaxiRide> greenTaxiRides = env.readTextFile("data/green_tripdata_2020-05(5am-8am).csv")
-                .map(new MapFunction<String, TaxiRide>() {
+        // read green Taxi data
+        DataStream<TaxiRide> greenTaxiRides = env.readTextFile("taxi_data/green_tripdata_2020-05(5am-8am).csv")
+                .flatMap(new FlatMapFunction<String, TaxiRide>() {
+                    private boolean isFirstLine = true;
                     @Override
-                    public TaxiRide map(String value) throws ParseException {
-                        String[] fields = value.split(",");
-                        return new TaxiRide("green", Double.parseDouble(fields[0]), fields[1], fields[2],
-                                Double.parseDouble(fields[8]), Double.parseDouble(fields[16]), 0);
+                    public void flatMap(String s, Collector<TaxiRide> collector) throws ParseException {
+                        if (isFirstLine) {
+                            isFirstLine = false;
+                        } else {
+                            String[] fields = s.split(";");
+                            TaxiRide ride = new TaxiRide("green", Double.parseDouble(fields[0]), fields[1], fields[2],
+                                    Double.parseDouble(fields[8]), Double.parseDouble(fields[16]), 0);
+                            collector.collect(ride);
+                        }
                     }
                 })
                 .assignTimestampsAndWatermarks(orderWatermarkStrategy);
 
         // 将模式应用到数据流
         DataStream<TaxiRide> allTaxiRides = yellowTaxiRides.union(greenTaxiRides);
-        allTaxiRides.print();
-        // 定义一个模式
-        Pattern<TaxiRide, ?> pattern = Pattern.<TaxiRide>begin("start")
-                .where(new SimpleCondition<TaxiRide>() {
+
+        // Pattern: triggering a CE when yellow and green taxis with vendorID 1 appear simultaneously within a ten-minute window
+        AfterMatchSkipStrategy skipStrategy = AfterMatchSkipStrategy.noSkip();
+        Pattern<TaxiRide, ?> pattern = Pattern.<TaxiRide>begin("start", skipStrategy)
+                .where(new IterativeCondition<TaxiRide>() {
                     @Override
-                    public boolean filter(TaxiRide ride) {
-                        return ride.getColor().equals("yellow") && ride.getVendorID() == 1;
+                    public boolean filter(TaxiRide ride, Context<TaxiRide> ctx) throws Exception {
+                        return (ride.getColor().equals("yellow") && ride.getVendorID() == 1 || ride.getColor().equals("green")) && ride.getVendorID() == 1;
                     }
                 })
-                .where(new SimpleCondition<TaxiRide>() {
-                    @Override
-                    public boolean filter(TaxiRide ride) {
-                        return ride.getColor().equals("green") && ride.getVendorID() == 1;
-                    }
-                })
+                .times(2)  // 期待两个匹配事件
+//                .where(new SimpleCondition<TaxiRide>() {
+//                    @Override
+//                    public boolean filter(TaxiRide ride) {
+//                        return ride.getColor().equals("yellow") && ride.getVendorID() == 1;
+//                    }
+//                })
+//                .where(new SimpleCondition<TaxiRide>() {
+//                    @Override
+//                    public boolean filter(TaxiRide ride) {
+//                        return ride.getColor().equals("green") && ride.getVendorID() == 1;
+//                    }
+//                })
                 .within(Time.minutes(10));
 
         // 将模式应用到数据流上
-        PatternStream<TaxiRide> patternStream = CEP.pattern(allTaxiRides.keyBy(TaxiRide::getVendorID), pattern);
+        //PatternStream<TaxiRide> patternStream = CEP.pattern(allTaxiRides.keyBy(TaxiRide::getColor), pattern);
+        PatternStream<TaxiRide> patternStream = CEP.pattern(allTaxiRides, pattern);
+
 
         // 从匹配的模式中选择结果
-        DataStream<String> result = patternStream.select(new PatternSelectFunction<TaxiRide, String>() {
-            @Override
-            public String select(Map<String, List<TaxiRide>> pattern) {
-                TaxiRide yellowRide = pattern.get("yellow").get(0);
-                TaxiRide greenRide = pattern.get("green").get(0);
-                return "Match found! VendorID: " + yellowRide.getVendorID() +
-                        ", Yellow Pickup: " + yellowRide.getPickupDatetime() +
-                        ", Green Pickup: " + greenRide.getPickupDatetime();
+        DataStream<String> result = patternStream.process(new PatternProcessFunction<TaxiRide, String>() {
+            @Override//Called every time a matching sequence of events is found.
+            public void processMatch(Map<String, List<TaxiRide>> match, Context context, Collector<String> collector) throws Exception {
+                StringBuilder matches = new StringBuilder();
+                List<TaxiRide> rides = match.get("start");
+                TaxiRide yellowRide = rides.get(0);
+                TaxiRide greenRide = rides.get(1);
+
+                String s = "Yellow taxi pickup at_" + yellowRide.getPickupDatetime() + "_drop at_" + yellowRide.getDropoffDatetime() +
+                        "_with Green taxi at_" + greenRide.getPickupDatetime() + "_drop at_" + greenRide.getDropoffDatetime();
+                collector.collect(s);
             }
         });
 
         result.print();
+        String outputPath = "taxi_result/result1.csv";
+        result.writeAsText(outputPath, FileSystem.WriteMode.OVERWRITE);
+        //create csv datei
+//        final StreamingFileSink<String> sink = StreamingFileSink
+//                .forRowFormat(new Path(outputPath), new SimpleStringEncoder<String>("UTF-8"))
+//                .withRollingPolicy(DefaultRollingPolicy.builder()
+//                        .withRolloverInterval(Duration.ofMinutes(1_000).toMillis())
+//                        .withInactivityInterval(Duration.ofMinutes(1_000).toMillis())
+//                        .withMaxPartSize(1024 * 1024 * 1024)
+//                        .build())
+//                .build();
+//
+//        result.addSink(sink);
 
         // 执行Flink作业
         env.execute("Taxi Ride CEP Example");
--- src/main/java/example/TaxiRide.java
+++ src/main/java/example/TaxiRide.java
@@ -2,7 +2,8 @@ package example;
 
 import java.util.Date;
 import java.text.SimpleDateFormat;
-import  java.text.ParseException;
+import java.text.ParseException;
+
 public class TaxiRide {
     private String color;
     private double vendorID;
@@ -11,9 +12,9 @@ public class TaxiRide {
     private double distance;
     private double totalAmount;
     private double congestion_surcharge;
-    public TaxiRide(){}
+    public TaxiRide() {}
 
-    public TaxiRide(String color, double vendorID, String pickupDatetime, String dropoffDatetime, double distance, double totalAmount,double congestion_surcharge) throws ParseException {
+    public TaxiRide(String color, double vendorID, String pickupDatetime, String dropoffDatetime, double distance, double totalAmount, double congestion_surcharge) throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         this.color = color;
         this.vendorID = vendorID;
@@ -24,12 +25,11 @@ public class TaxiRide {
         this.congestion_surcharge = congestion_surcharge;
     }
 
-    public String toString(){
-        return color + pickupDatetime;
-    }
-    public String getColor(){
+    // Getters
+    public String getColor() {
         return color;
     }
+
     public double getVendorID() {
         return vendorID;
     }
@@ -50,4 +50,52 @@ public class TaxiRide {
         return totalAmount;
     }
 
+    public double getCongestionSurcharge() {
+        return congestion_surcharge;
+    }
+
+    // Setters
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setVendorID(double vendorID) {
+        this.vendorID = vendorID;
+    }
+
+    public void setPickupDatetime(String pickupDatetime) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        this.pickupDatetime = sdf.parse(pickupDatetime);
+    }
+
+    public void setDropoffDatetime(String dropoffDatetime) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        this.dropoffDatetime = sdf.parse(dropoffDatetime);
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public void setCongestionSurcharge(double congestion_surcharge) {
+        this.congestion_surcharge = congestion_surcharge;
+    }
+
+    @Override
+    public String toString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return "TaxiRide{" +
+                "color='" + color + '\'' +
+                ", vendorID=" + vendorID +
+                ", pickupDatetime=" + sdf.format(pickupDatetime) +
+                ", dropoffDatetime=" + sdf.format(dropoffDatetime) +
+                ", distance=" + distance +
+                ", totalAmount=" + totalAmount +
+                ", congestion_surcharge=" + congestion_surcharge +
+                '}';
+    }
 }
